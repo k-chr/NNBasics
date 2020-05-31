2f5d22e0c3137c58aaa4ae676337f5ad86378a97
Kamil Chrustowski <chrustek123456789@gmail.com>
2020-05-31 02:12:24 +0200

    Added struct FlatMatrix

---
+++ NNBasicsUtilities/Core/Utilities/UtilityTypes/FlatMatrix.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Text;
+
+namespace NNBasicsUtilities.Core.Utilities.UtilityTypes
+{
+	public struct FlatMatrix
+	{
+		private readonly double[] _data;
+		public readonly int Rows;
+		public readonly int Cols;
+
+		public override string ToString()
+		{
+			var builder = new StringBuilder();
+			for (var i = 0; i < _data.Length; ++i)
+			{
+				if (i % (Cols) == 0)
+				{
+					builder.Append("| ");
+				}
+				builder.Append(_data[i]).Append(' ');
+				if ((i + 1) % (Cols) == 0)
+				{
+					builder.Append("|\n");
+				}
+			}
+
+			return builder.ToString();
+		}
+
+
+
+	}
+}
