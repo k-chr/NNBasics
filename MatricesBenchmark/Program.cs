5671f694af3f95f3ba921a292578e06a25803b08
Kamil Chrustowski <chrustek123456789@gmail.com>
2020-06-13 15:36:49 +0200

    Added benchmarking project

---
+++ MatricesBenchmark/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using BenchmarkDotNet.Running;
+namespace MatricesBenchmark
+{
+   class Program
+   {
+	  static void Main(string[] args)
+	  {
+		 Console.WriteLine("Hello World!");
+		 BenchmarkRunner.Run<Benchmarking>();
+	  }
+   }
+}
