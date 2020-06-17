df1f7b7d1f20d7b110b05152d45ba3c6dc790f65
Kamil Chrustowski <chrustek123456789@gmail.com>
2020-06-17 03:22:37 +0200

    Added some utilities for mutations

--- GeneticAlgorithms/test.py
+++ GeneticAlgorithms/test.py
@@ -6,10 +6,12 @@ Created on Wed Jun 17 02:14:39 2020
 """
 
 
-from utils import chromosome_inversion
+from utils import *
 from numpy import array
 
 arr = array([0,0,0,1,1,0,0,1,0,1])
 print(arr)
 chromosome_inversion(arr)
 print(arr)
+l = [arr,]
+roulette_wheel_selection(l, count_bits)
--- GeneticAlgorithms/utils.py
+++ GeneticAlgorithms/utils.py
@@ -5,11 +5,12 @@ Created on Wed Jun 17 01:40:56 2020
 @author: Kamil Chrustowski
 """
 from numpy import *
+from collections import *
 from random import randint
 """
 It does the chromosome inversion of existing genom
 """
-def chromosome_inversion(genome:array)->array:
+def chromosome_inversion(genome:array):
     if(len(genome) < 1): 
         return
     i = randint(0, len(genome) - 1)
@@ -18,3 +19,31 @@ def chromosome_inversion(genome:array)->array:
         j = randint(0, len(genome) - 1)
     i, j = (i, j) if i < j else (j, i)
     genome[i:j] = array(array(genome[i:j])[::-1])
+    
+def adjacent_swap(genome: array):
+    i = randint(0, len(genome) - 2)
+    tmp = genome[i]
+    genome[i] = genome[i+1]
+    genome[i+1] = tmp
+    
+def random_swap(genome:array):
+    i = randint(0, len(genome) - 1)
+    j = randint(0, len(genome) - 1)
+    while i == j:
+        j = randint(0, len(genome) - 1)
+    tmp = genome[i]
+    genome[i] = genome[j]
+    genome[j] = tmp
+    
+def random_replacement(genome: array):
+    i = randint(0, len(genome) - 1)
+    genome[i] = 1 - genome[i]
+    
+def roulette_wheel_selection(population:list, fitness_function):
+    wheel = defaultdict()
+    fit_values = [(genome, fitness_function(genome)) for genome in population]
+    fit_sum = sum([values[1] for values in population])
+    
+def count_bits(genome: array):
+    
+    return count_nonzero(genome)
